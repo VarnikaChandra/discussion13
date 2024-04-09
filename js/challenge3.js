1517410c7d3035ed4c2c1cde256ff5b0c088a803
VarnikaChandra <123506866+VarnikaChandra@users.noreply.github.com>
2024-04-09 17:49:08 -0400

    a
    g

--- js/challenge3.js
+++ js/challenge3.js
@@ -5,5 +5,5 @@ window.addEventListener("load",function(){//load event listener
     //inner html: gives content as html with tags
     //.textContent only gives the actual text content without the html tags
     //console.log(document.querySelectorAll('figcaption'));//for all matching queries 
-    alert(document.querySelectorAll('figcaption')[1].innerHtml);
+    alert(document.querySelectorAll('figcaption')[1].innerHTML);
 });
